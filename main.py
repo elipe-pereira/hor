ab7bb26db3f95750584f944213c32dd0fc9770af
Eli <eli@hanokh.com.br>
2023-07-30 16:15:37 -0300

    Ajuste de teste6

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class Main:
 
 
 if __name__ == "__main__":
-    base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    base_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
     os.chdir(base_dir)
     print(base_dir)
     app = Main()
