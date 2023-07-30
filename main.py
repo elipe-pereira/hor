932e6f26c80b6d83e9c9f495bfdf4d9c6726f9e2
Eli <eli@hanokh.com.br>
2023-07-30 16:51:01 -0300

    Ajuste e remoção do código de teste

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from configparser import ConfigParser
 
 class Main:
     def __init__(self):
-        self.base_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+        self.base_dir = os.path.dirname(os.path.realpath(__file__))
         self.config = ConfigParser()
         self.config.read(self.base_dir + "/conf/hor.conf")
         self.config_sections = self.config.sections()
@@ -33,8 +33,5 @@ class Main:
 
 
 if __name__ == "__main__":
-    base_dir = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(base_dir)
-    print(base_dir)
     app = Main()
     app.run()
