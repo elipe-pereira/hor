53a0231b02bdc734fc4373fb9b9b38f9eca6e2a2
Eli <eli@hanokh.com.br>
2023-07-30 03:18:56 -0300

    Ajustado spec_path em pyinstaller.sh e adicionado comandos no pipeline

--- pyinstaller.sh
+++ pyinstaller.sh
@@ -2,8 +2,10 @@
 base_dir=`pwd`
 dist_path="."
 work_path="/tmp"
-spec_path="/tmp/hor"
+spec_path="/tmp/build/hor"
 name="hor"
+
+mkdir -p $spec_path
 
 pyinstaller --distpath $dist_path\
         --add-data "$base_dir/conf:conf"\
